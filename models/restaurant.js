c1d12cec7c7a6833b8bf7d64132ceb374f542b4e
Appleeway <applee62002@gmail.com>
2020-02-27 01:48:04 +0800

    Fix model phone type

--- models/restaurant.js
+++ models/restaurant.js
@@ -2,21 +2,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const restaurantSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
+  name: { type: String, required: true },
   name_en: String,
   category: String,
   image: [String],
   location: String,
-  phone: Number,
+  phone: String,
   google_map: [String],
   rating: Number,
-  description: {
-    type: String,
-    required: true
-  }
+  description: { type: String, required: true }
 })
 
 module.exports = mongoose.model('Restaurant', restaurantSchema)
