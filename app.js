48499b1a9b83e072375b37f4904b915bba162d27
Appleeway <applee62002@gmail.com>
2020-02-26 23:44:21 +0800

    Add mongoose and set connection

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 //require packages used in the project 
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const restaurantList = require('./restaurant.json')
 
 //constant value
 const app = express()
 const port = 3000
+
+//setting mongodb
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+//連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+//連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 //setting express-handlebars here
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
