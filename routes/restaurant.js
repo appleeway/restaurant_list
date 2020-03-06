4b92742d8e31760a52df4d0a98230a61fb5bf700
Appleeway <applee62002@gmail.com>
2020-03-06 15:56:09 +0800

    Add user registeration feature and modify restaurant model syntax

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -53,15 +53,8 @@ router.get('/:id/edit', (req, res) => {
 router.put('/:id', (req, res) => {
   Restaurant.findById(req.params.id, (err, restaurant) => {
     if (err) return console.error(err)
-    restaurant.name = req.body.name,
-      restaurant.name_en = req.body.name_en,
-      restaurant.category = req.body.category,
-      restaurant.image = req.body.image,
-      restaurant.location = req.body.location,
-      restaurant.phone = req.body.phone,
-      restaurant.google_map = req.body.google_map,
-      restaurant.rating = req.body.rating,
-      restaurant.description = req.body.description
+    const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
+
     restaurant.save(err => {
       if (err) return console.error(err)
       return res.redirect(`/restaurants/${req.params.id}`)
