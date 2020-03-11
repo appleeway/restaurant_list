28cf35c0bf9d06f2435c986873fd114fe4fb92b1
Appleeway <applee62002@gmail.com>
2020-03-11 18:54:46 +0800

    Add connect-flash for UI messages

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('passport')
+const flash = require('connect-flash')
 
 //constant value
 const app = express()
@@ -54,11 +55,17 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+// 使用connect flash
+app.use(flash())
 
-//登入後可以取得使用者的資訊方便我們在view裡面使用
+// 登入後可以取得使用者的資訊方便我們在view裡面使用
 app.use((req, res, next) => {
   res.locals.user = req.user
   res.locals.isAuthenticated = req.isAuthenticated()
+
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+
   next()
 })
 
